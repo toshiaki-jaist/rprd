f336e4a1b096c707d68e4ab1430d4161131887f0
toshiaki-jaist <99782314+toshiaki-jaist@users.noreply.github.com>
2024-02-21 15:14:30 +0900

    Update case3_2.py

--- experiment/case3_2.py
+++ experiment/case3_2.py
@@ -21,10 +21,10 @@ case32.append_ntrans ([("LCar", 0, "LCar", 1), ("LCar", 0, "LCar", 2), ("LCar", 
 case32.append_ntrans ([("RCar1", 0, "RCar1", 1), ("RCar1", 1, "RCar1", 2), ("RCar1", 2, "RCar1", 3), ("RCar1", 2, "RCar1", 4), ("RCar1", 2, "RCar1", 5)])
 case32.append_ctrans ([("RCar2", 0, "RCar2", 1, "RCar1", 5), ("RCar2", 0, "RCar2", 2, "RCar1", 4), ("RCar2", 0, "RCar2", 3, "RCar1", 3)])
 case32.append_ctrans ([("EgoCar", 0, "EgoCar", 1, "RCar1", 1), ("EgoCar", 2, "EgoCar", 4, "RCar1", 4), ("EgoCar", 4, "EgoCar", 5, "LCar", 2)])
-case32.append_netrans ([("EgoCar", 0, "EgoCar", 7, "RCar1", 1)])
-case32.append_netrans ([("EgoCar", 2, "EgoCar", 3, "RCar1", 4)])
-case32.append_netrans ([("EgoCar", 4, "EgoCar", 6, "LCar", 2)])
-case32.append_netrans ([("EgoCar", 0, "EgoCar", 7, "RCar1", 1)])
+case32.append_netrans ([("EgoCar", 0, "EgoCar", 7, [("RCar1", 1)])])
+case32.append_netrans ([("EgoCar", 2, "EgoCar", 3, [("RCar1", 4)])])
+case32.append_netrans ([("EgoCar", 4, "EgoCar", 6, [("LCar", 2)])])
+case32.append_netrans ([("EgoCar", 0, "EgoCar", 7, [("RCar1", 1)])])
 
 if True:
     # count the number of collision scenarios
