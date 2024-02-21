31c3f9ce00fc91356f5b329aabbabc33a6cbb268
toshiaki-jaist <99782314+toshiaki-jaist@users.noreply.github.com>
2024-02-21 15:13:04 +0900

    Update gcpd.py
    Flexibly adding conditions. Chenge add_netrans

--- gcpd.py
+++ gcpd.py
@@ -22,6 +22,7 @@ class Model:
         self.ntrans = []
         self.ctrans = []
         self.netrans = []
+        self.cstrans = []
         self.strans = []
         self.max_step = 0
         self.num_model = 0
@@ -50,6 +51,10 @@ class Model:
         self.position = ps
     def get_position(self):
         return self.position
+    def get_position(self, c, n):
+        for (c1,n1,p) in self.position:
+            if c1 ==c and n1 == n:
+                return p
     def append_position(self,ps):
         self.position = self.position + ps
     def add_position(self,p):
@@ -59,6 +64,11 @@ class Model:
         self.lane = ls
     def get_lane(self):
         return self.lane
+    def get_lane(self, c, n):
+        for (c1,n1,l) in self.lane:
+            if c1 == c and n1 == n:
+                return l
+
     def append_lane(self,ls):
         self.lane = self.lane + ls
     def add_lane(self,l):
@@ -81,6 +91,8 @@ class Model:
         self.ntrans = self.ntrans + ts
     def add_ntrans(self,t):
         self.ntrans.append(t)
+    def remove_ntrans(self,t):
+        self.ntrans.remove(t)
 
     def set_ctrans(self,ts):
         self.ctrans = ts
@@ -91,15 +103,38 @@ class Model:
     def add_ctrans(self,t):
         self.ctrans.append(t)
 
+    # t: (c1,n1,c2,n2,[(cn,nn)])
     def set_netrans(self,ts):
         self.netrans = ts
     def get_netrans(self):
         return self.netrans
+    def get_netrans4(self, c1, n1, c2, n2):
+        for (c3, n3, c4, n4, cbx) in self.netrans:
+            if c1 == c3 and n1 == n3 and c2 == c4 and n2 == n4:
+                return (c3, n3, c4, n4, cbx)
     def append_netrans(self,ts):
         self.netrans = self.netrans + ts
     def add_netrans(self,t):
         self.netrans.append(t)
+    def remove_netrans(self,t):
+        self.netrans.remove(t)
+    def check_netrans4(self, c1, n1, c2, n2):
+        flag = False
+        for (c3, n3, c4, n4, cbx) in self.netrans:
+            if c1 == c3 and n1 == n3 and c2 == c4 and n2 == n4:
+                flag = True
+        return flag
         
+    # t: (c1,n1,c2,n2,f)
+    def set_cstrans(self,ts):
+        self.cstrans = ts
+    def get_cstrans(self):
+        return self.cstrans
+    def append_cstrans(self,ts):
+        self.cstrans = self.cstrans + ts
+    def add_cstrans(self,t):
+        self.cstrans.append(t)
+
     def set_strans(self,ts):
         self.strans = ts
     def get_strans(self):
@@ -108,6 +143,28 @@ class Model:
         self.strans = self.strans + ts
     def add_strans(self,t):
         self.strans.append(t)
+
+    def check_loc (self, c, l, p):
+        flag = False
+        for (b1, n1, x) in self.lane:
+            for (b2, n2, y) in self.position:
+                if ( c == b1 and c == b2 and n1 == n2 and x == l and y == p):
+                    flag = True
+                    break
+        return flag
+
+    def get_loc (self, c, l, p):
+        for (b1, n1, x) in self.lane:
+            for (b2, n2, y) in self.position:
+                if ( c == b1 and c == b2 and n1 == n2 and x == l and y == p):
+                    return (b1, n2)
+
+    def check_ntrans(self, c1, n1, c2, n2):
+        flag = False
+        for (c3,n3,c4,n4) in self.ntrans:
+            if (c1 == c3 and c2 == c4 and n1 == n3 and n2 == n4):
+                flag = True
+        return flag
 
 def init(m):
     i = 0
@@ -155,10 +212,26 @@ def ps_ctrans(ts,bx,s):
 # generating constraints for conditional (not exist) transitions
 def ps_netrans(ts,bx,s):
     ps = []
-    for (c1,n1,c2,n2,c3,n3) in ts:
+    for (c1,n1,c2,n2,cbx) in ts:
         props = []
         props.append(Box (c2i[c1], n1, s))
-        props.append(Not(Box (c2i[c3], n3, s)))
+        props.append(Not (Box(c2i[c1], n1, s+1)))
+        props.append(Box (c2i[c2], n2, s+1))
+        for (c,n) in cbx:
+            props.append(Not(Box (c2i[c], n, s)))
+        for (c, n) in bx:
+            if not ((c1, n1) == (c,n) or (c2,n2) == (c,n)):
+                props.append(Box (c2i[c],n,s) == Box (c2i[c],n,s+1))
+        ps.append(And (props))
+    return ps
+
+#
+def ps_cstrans(ts,bx,s):
+    ps = []
+    for (c1,n1,c2,n2, f) in ts:
+        props = []
+        props.append(Box (c2i[c1], n1, s))
+        props.append(f (s) )
         props.append(Not (Box(c2i[c1], n1, s+1)))
         props.append(Box (c2i[c2], n2, s+1))
         for (c, n) in bx:
@@ -184,14 +257,19 @@ def ps_strans(tss, bx, s):
     return ps
     
 # generating constraints for no transition
-def p_disable (ts, cs, nes, sss, bx, s):
+def p_disable (ts, cs, nes, cst, sss, bx, s):
     props1 = []
     for (c1,n1,c2,n2) in ts:
         props1.append(Not(Box(c2i[c1],n1,s)))
     for (c1,n1,c2,n2,c3,n3) in cs:
         props1.append(Not(And(Box(c2i[c1],n1,s), Box(c2i[c3],n3,s))))
-    for (c1,n1,c2,n2,c3,n3) in nes:
-        props1.append(Not(And(Box(c2i[c1],n1,s), Not(Box(c2i[c3],n3,s)))))
+    for (c1,n1,c2,n2,cbx) in nes:
+        ps = [Box(c2i[c1],n1,s)]
+        for (c,n) in cbx:
+            ps.append(Not(Box(c2i[c],n,s)))
+        props1.append(Not(And(ps)))
+    for (c1,n1,c2,n2, f) in cst:
+        props1.append(Not(And(Box(c2i[c1],n1,s), f (s))))
     for ss in sss:
         ps = []
         for (c1,n1,c2,n2) in ss:
@@ -205,7 +283,20 @@ def p_disable (ts, cs, nes, sss, bx, s):
 #def add_trans(ts, cs, nes, ss, bx, n):
 def add_trans(m):
     for i in range(m.max_step):
-        solver.add(Or (ps_ntrans(m.ntrans,m.boxes,i) + ps_ctrans(m.ctrans, m.boxes,i) + ps_netrans(m.netrans,m.boxes,i) + ps_strans(m.strans,m.boxes,i) + [p_disable(m.ntrans, m.ctrans, m.netrans, m.strans, m.boxes, i)]))
+        solver.add(Or (ps_ntrans(m.ntrans,m.boxes,i) + ps_ctrans(m.ctrans, m.boxes,i) + ps_netrans(m.netrans,m.boxes,i) + ps_cstrans(m.cstrans,m.boxes,i) + ps_strans(m.strans,m.boxes,i) + [p_disable(m.ntrans, m.ctrans, m.netrans, m.cstrans, m.strans, m.boxes, i)]))
+
+def all_disable(m, s):
+    prop = []
+    for (c, n) in m.boxes:
+        prop.append(Box (c2i[c],n,s) == Box (c2i[c],n,s+1))
+    return (And(prop))
+        
+def add_trans_tm(m,cnd):
+    for i in range(m.max_step):
+        trans = Or (ps_ntrans(m.ntrans,m.boxes,i) + ps_ctrans(m.ctrans, m.boxes,i) + ps_netrans(m.netrans,m.boxes,i) + ps_cstrans(m.cstrans,m.boxes,i) + ps_strans(m.strans,m.boxes,i) + [p_disable(m.ntrans, m.ctrans, m.netrans, m.cstrans, m.strans, m.boxes, i)])
+        c_trans = And(Not(cnd(m.boxes, i)), trans)
+        n_trans = And(cnd(m.boxes, i), all_disable(m,i))
+        solver.add(Or(c_trans, n_trans))
 
 #add position constraints
 def add_pos(m):
@@ -240,37 +331,98 @@ def add_lane(m):
         else:
             print ("position or lane constraints are unsatisfiable")
 
-#add collision constraints
-def ps_col(bx, ms):
+#collision primitives
+def ps_col(c1, c2, bx, t):
     ps = []
-    for t in range(ms + 1):
-        for (c1,b1) in bx:
-            for (c2,b2) in bx:
-                if not (c1 == c2 and b1 == b2):
-                    ps.append(And(Box(c2i[c1], b1, t), Box(c2i[c2], b2, t), Pos(c2i[c1], b1) == Pos(c2i[c2], b2), Lane(c2i[c1], b1) == Lane(c2i[c2], b2)))
-    return ps
+    for b1 in [b for (c,b) in bx if c == c1]:
+        for b2 in [b for (c,b) in bx if c == c2]:
+            ps.append(And(Box(c2i[c1], b1, t), Box(c2i[c2], b2, t), Pos(c2i[c1], b1) == Pos(c2i[c2], b2), Lane(c2i[c1], b1) == Lane(c2i[c2], b2)))
+    return Or (ps)
 
-def ps_colc(bx, ms, c1, c2):
+def ps_lp(c1, bx, l, p, t):
     ps = []
-    for t in range(ms + 1):
-        for (c3,b3) in bx:
-            for (c4,b4) in bx:
-                if (c1 == c3 and c2 == c4):
-                    ps.append(And(Box(c2i[c3], b3, t), Box(c2i[c4], b4, t), Pos(c2i[c3], b3) == Pos(c2i[c4], b4), Lane(c2i[c3], b3) == Lane(c2i[c4], b4)))
-    return ps
+    for b1 in [b for (c,b) in bx if c == c1]:
+        ps.append(And(Box(c2i[c1], b1, t), Pos(c2i[c1], b1) == p, Lane(c2i[c1], b1) == l))
+    return Or(ps)
+        
+def ps_front(c1, bx, t):
+    ps = []
+    for b1 in [b for (c,b) in bx if c == c1]:
+        for b2 in [b for (c,b) in bx if c == c1]:
+            if not (b1 == b2):
+                ps.append(And(0 < t, Box(c2i[c1], b1, t - 1), Box(c2i[c1], b2, t), Pos(c2i[c1], b1) < Pos(c2i[c1],b2)))
+    return (Or (ps))
+
+def ps_rear(c1, bx, t):
+    ps = []
+    for b1 in [b for (c,b) in bx if c == c1]:
+        for b2 in [b for (c,b) in bx if c == c1]:
+            if not (b1 == b2):
+                ps.append(And(0 < t, Box(c2i[c1], b1, t - 1), Box(c2i[c1], b2, t), Pos(c2i[c1], b1) > Pos(c2i[c1],b2)))
+    return (Or (ps))
+
+def ps_right(c1, bx, t):
+    ps = []
+    for b1 in [b for (c,b) in bx if c == c1]:
+        for b2 in [b for (c,b) in bx if c == c1]:
+            if not (b1 == b2):
+                ps.append(And(0 < t, Box(c2i[c1], b1, t - 1), Box(c2i[c1], b2, t), Lane(c2i[c1], b1) < Lane(c2i[c1],b2)))
+    return (Or (ps))
+
+def ps_left(c1, bx, t):
+    ps = []
+    for b1 in [b for (c,b) in bx if c == c1]:
+        for b2 in [b for (c,b) in bx if c == c1]:
+            if not (b1 == b2):
+                ps.append(And(0 < t, Box(c2i[c1], b1, t - 1), Box(c2i[c1], b2, t), Lane(c2i[c1], b1) > Lane(c2i[c1],b2)))
+    return (Or (ps))
+
+# exists c1 c2 t. f(c1, c2, bx, t)
+def eval_col (m, f):
+    ps = []
+    for c1 in m.cars:
+        for c2 in m.cars:
+            if not (c1 == c2):
+                for t in range (m.max_step + 1):
+                    ps.append(f(c1, c2, m.boxes, t))
+    return Or(ps)
+
+# exists t. f(bx, t)
+def eval_col_t (m, f):
+    ps = []
+    for t in range (m.max_step + 1):
+        ps.append(f(m.boxes, t))
+    return Or(ps)
+
+def eval_all_t (m, f):
+    ps = []
+    for t in range (m.max_step + 1):
+        ps.append(f(m.boxes, t))
+    return (And(ps))
+
+# exists c t. f(c, bx, t)
+def eval_col_c (m, f):
+    ps = []
+    for c in m.cars:
+        for t in range (m.max_step + 1):
+            ps.append(f(c, m.boxes, t))
+    return (Or(ps))
+
+def add_solver (c):
+    solver.add(c)
 
 def add_col(m):
-    solver.add(Or(ps_col(m.boxes, m.max_step)))
+    add_solver(eval_col(m, lambda c1, c2, bx, t: ps_col(c1, c2, bx, t)))
 
 def add_noncol(m):
-    solver.add(Not(Or(ps_col(m.boxes, m.max_step))))
+    add_solver(Not(eval_col(m, lambda c1, c2, bx, t: ps_col(c1, c2, bx, t))))
 
 # collision of c1 and c2
 def add_colc(m, c1, c2):
-    solver.add(Or(ps_colc(m.boxes, m.max_step, c1, c2)))    
+    add_solver(eval_col_t(m, lambda bx, t: ps_col(c1, c2, bx, t)))
 
 def add_noncolc(m, c1, c2):
-    solver.add(Not(Or(ps_colc(m.boxes, m.max_step, c1, c2))))
+    add_solver(Not(eval_col_t(m, lambda bx, t: ps_col(c1, c2, bx, t))))
 
 #count the number of scenarios
 def enum_count(m):
@@ -360,6 +512,31 @@ def print_constraints():
     for c in solver.assertions():
         print (c)
 
+def print_model(m):
+    print ("Cars")
+    print (m.cars)
+    print ("Boxes")
+    print (m.boxes)
+    print ("Position")
+    print (m.position)
+    print ("Lane")
+    print (m.lane)
+    print ("Inits")
+    print (m.inits)
+    print ("Ntrans")
+    print (m.ntrans)
+    print ("Ctrans")
+    print (m.ctrans)
+    print ("Netrans")
+    print (m.netrans)
+    print ("Strans")
+    print (m.strans)
+    print ("Max Step")
+    print (m.max_step)
+    print ("Num Model")
+    print (m.num_model)
+
+
 #c: cars, p: pos, l: lane, ib: init_box, bx: box
 #nt:ntrans, ct:ctrans, net; netrans, st:strans
 #nm: NUM_MODEL, ms: MAX_STEP
@@ -389,3 +566,11 @@ def add_constraints(m):
     add_lane(m)
     add_init(m)
     add_trans(m)
+
+# terminates if cnd becomes true
+def add_constraints_tm(m,cnd):
+    init(m)
+    add_pos(m)
+    add_lane(m)
+    add_init(m)
+    add_trans_tm(m,cnd)
